7eef3acdfaf63436a8d257c3a4215b43c763d8a1
makinibryan <bmakini@ymail.com>
2021-08-20 13:52:45 +0100

    Added the client login and register to the app

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { error } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/User';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -14,24 +16,20 @@ export class AppComponent implements OnInit {
   /**
    *
    */
-  constructor(private http: HttpClient) {  }
+  constructor( private accountService: AccountService) {  }
 
 
   ngOnInit() {
-    this.getUsers();
+    
+    this.setCurrentUser();
+  }
+  setCurrentUser()
+  {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
 
-  getUsers()
-  {
-    this.http.get('https://localhost:5001/api/users').subscribe(response =>
-    {
-      this.users = response;
-    },
-    error =>
-    {
-      console.log(error);
-    })
-  }
+  
   
 
   
