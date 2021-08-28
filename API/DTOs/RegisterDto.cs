a39e0d388bb5b92e3f66191d57c70e81a3b96d4c
makinibryan <bmakini@ymail.com>
2021-08-28 21:24:00 +0100

    Added Middleware in the API

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -9,6 +9,7 @@ namespace API.DTOs
         public string Username { get; set; }
 
         [Required (ErrorMessage = "Password is required!")]
+        [StringLength(8, MinimumLength  = 4)]
         public string Password { get; set; }
     }
 }
