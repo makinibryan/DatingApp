6bbcdd077b212310cc6ccfa84684e39e64ef5c36
makinibryan <bmakini@ymail.com>
2021-08-30 03:53:17 +0100

    Created the repository and the service in the application service extension

---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface IUserRepository
+    {
+         void Update(AppUser User);
+         
+         Task<bool> SaveAllAsync();
+         Task<IEnumerable<AppUser>> GetUserAsync();
+         Task<AppUser> GetUserByIdAsync(int id);
+         Task<AppUser> GetUserByUsernameAsync(string username);
+
+    }
+}
